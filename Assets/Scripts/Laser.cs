9ea120a52cc28187d29604139a0605db61732b52
Ashoo Jindal <ashoo.jindal@gmail.com>
2022-04-10 17:14:15 +0530

    Upload

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+
+    private int maxReflections = 20;
+
+    private int posCount;
+    private LineRenderer lr;
+
+    [SerializeField]
+    private Vector3 _offSet;
+
+    private void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        lr.positionCount = maxReflections;
+
+    }
+    private void Update()
+    {
+        posCount = 0;
+        ReflectRay(transform.position + _offSet, transform.forward);
+    }
+    private void ReflectRay(Vector3 position, Vector3 direction)
+    {
+        lr.SetPosition(0, transform.position + _offSet);
+
+        for (int i = 0; i < maxReflections; i++)
+        {
+
+            Ray ray = new Ray(position, direction);
+            RaycastHit hit;
+
+            if (posCount < maxReflections - 1)
+                posCount++;
+
+            if (Physics.Raycast(ray, out hit, 300))
+            {
+
+                position = hit.point;
+                direction = Vector3.Reflect(direction, hit.normal);
+                lr.SetPosition(posCount, hit.point);
+
+                if (hit.transform.tag != "Mirror")
+                {
+                    for (int j = (i + 1); j < maxReflections; j++)
+                    {
+                        lr.SetPosition(j, hit.point);
+                    }
+                    break;
+                }
+                else
+                {
+
+                    lr.SetPosition(posCount, hit.point);
+                }
+            }
+            else
+            {
+
+                lr.SetPosition(posCount, ray.GetPoint(300));
+
+            }
+
+
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Rays.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rays : MonoBehaviour
+{
+    LineRenderer lr;
+    public int maxReflections = 20;
+    private Vector3 position;
+    private Vector3 direction;
+    private int pCount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        lr.SetPosition(0, transform.position);
+        lr.positionCount = maxReflections;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pCount = 0;
+        ReflectRay(transform.position, transform.forward);
+    }
+
+    void ReflectRay(Vector3 position, Vector3 direction)
+    {
+
+        lr.SetPosition(0, transform.position);
+
+        for (int i = 0; i < maxReflections; i++)
+        {
+            Ray ray = new Ray (position, direction);
+            RaycastHit hit;
+
+            if (pCount < maxReflections - 1)
+                pCount++;
+
+            if (Physics.Raycast(ray, out hit, 300))
+            {
+
+                position = hit.point;
+                direction = Vector3.Reflect(direction, hit.normal);
+                lr.SetPosition(pCount, hit.point);
+
+                if (hit.transform.tag != "Mirror")
+                {
+                    for (int j = (i + 1); j < maxReflections; j++)
+                    {
+                        lr.SetPosition(j, hit.point);
+                    }
+                    break;
+                }
+                else
+                {
+                    lr.SetPosition(pCount, hit.point);
+                }
+
+            }
+            else
+            {
+                lr.SetPosition(pCount, ray.GetPoint(5));
+            }
+        }
+
+    }
+
+}
